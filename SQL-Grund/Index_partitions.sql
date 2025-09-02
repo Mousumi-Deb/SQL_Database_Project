2f8d4f329aaf095c048ec549b0cd5d88cdca6490
Mousumi-Deb <mousumi.rani.deb@gmail.com>
2025-09-02 18:42:58 +0200

    added codes

--- SQL-Grund/Index_partitions.sql
+++ SQL-Grund/Index_partitions.sql
@@ -47,14 +47,14 @@ CREATE NONCLUSTERED COLUMNSTORE INDEX idx_DBCustomers_CS_FirstNAMe on sales.DBCu
 -----
 USE AdventureWorksDW2022;
 
-DROP TABLE IF EXISTS dbo.FactInternetSales ;
 
 --HEAP structures (It is a table without a clustered index)
-
+DROP TABLE IF EXISTS FactInternetSales_HP;
 SELECT * INTO FactInternetSales_HP 
 FROM FactInternetSales;
 
 --Row Stores (It is a table with a clustered index)
+DROP TABLE IF EXISTS FactInternetSales_RS;
 SELECT * INTO FactInternetSales_RS
 FROM FactInternetSales;
 
@@ -62,13 +62,18 @@ CREATE CLUSTERED INDEX idx_FactInternetSLaes_RS_PK
 ON FactInternetSales_RS(SalesOrdernumber, SalesOrderLineNumber)
 
 -----column Stores (It is a table with a clustered columnstore index)
+DROP TABLE IF EXISTS FactInternetSales_CS;
+
 SELECT * INTO FactInternetSales_CS
 FROM FactInternetSales;
 
-CREATE CLUSTERED COLUMNSTORE INDEX idx_FactInternetSales_CS_PK
-ON FactInternetSales_CS
+
+
+--CREATE CLUSTERED COLUMNSTORE INDEX idx_FactInternetSales_CS_PK
+--ON FactInternetSales_CS;
 
 --unique index ( Ensure no duplicates values exists in specific column)
+use SalesDB;
 create UNIQUE NONCLUSTERED index idx_Products_products  
 on Sales.Products (Product)
 
@@ -83,4 +88,75 @@ CREATE NONCLUSTERED INDEX idx_DBCustomers_Country
 ON Sales.Customers (Country)
 WHERE Country = 'USA'
 
----
+---list all  indexs on a specific tabel
+sp_helpindex 'Sales.DbCustomers'
+
+
+-- Monitoring Index Usage
+SELECT * from sys.indexes
+
+SELECT 
+    tbl.name as TableName,
+    idx.name as Index_name,
+    idx.type_desc as IndexType,
+    idx.is_primary_key  as IsPrimaryKey,
+    idx.is_unique as IsUnique,
+    idx.is_disabled as is_disabled,
+    s.user_seeks as UserSeeks,
+    s.user_scans as UserScans,
+    s.user_lookups as UserLook,
+    s.user_updates as UserUpdates,
+    COALESCE (s.last_user_seek,s.last_system_scan) LastUpdate
+
+FROM sys.indexes idx 
+JOIN sys.tables tbl 
+On idx.object_id = tbl.object_id
+LEFT JOIN sys.dm_db_index_usage_stats s  
+On s.object_id = idx.object_id
+AND s.index_id = idx.index_id
+
+ORDER BY tbl.name, idx.name
+
+--- Dynamic management view(DMV) provides realtime insights into database performence and system health
+
+select * from sys.dm_db_index_usage_stats
+
+--SELECT * FROM FactInternetSales
+
+SELECT * from sys.dm_db_missing_index_details
+
+USE AdventureWorksDW2022;
+SELECT * FROM dbo.FactInternetSales
+
+SELECT
+    fs.SalesOrdernumber,
+    dp.Color
+  FROM [AdventureWorksDW2022].[dbo].[FactInternetSales] fs
+INNER JOIN DimProduct dp  
+On fs.ProductKey = dp.ProductKey
+WHERE dp.Color ='Black'
+AND fs.OrderDateKey BETWEEN 20101229 AND 20101231
+
+SELECT * from sys.dm_db_missing_index_details 
+
+--- updates statistics
+USe SalesDB;
+SELECT
+    SCHEMA_NAME(t.schema_id) as SchemaName,
+    t.name as tablename,
+    s.name as StatisticsName,
+    sp.last_updated as last_Updated,
+    DATEDIFF(day, sp.last_updated,  GETDATE()) as LastUpdateDay, 
+    sp.rows as 'Rows',
+    sp.modification_counter as ModificationSinceLastUpdates
+FROM sys.stats as s
+JOIN sys.tables t 
+on s.object_id = t.object_id
+Cross APPLY sys.dm_db_stats_properties(s.object_id, s.stats_id) as sp  
+order by 
+sp.modification_counter desc;
+
+UPDATE STATISTICS Sales.DBCustomers _WA_Sys_00000001_6EF57B66;
+
+UPDATE STATISTICS Sales.DBCustomers;
+
